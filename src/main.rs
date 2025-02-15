f9ca435b30f2fb38b41217a76843ea947b8538d1
Fae Hamilton-Raine <faehamiltonraine@gmail.com>
2025-02-15 15:59:44 -0700

    modules

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,7 @@
-#[derive(Debug)]
-enum Media {
-    Book { title: String, author: String },
-    Movie { title: String, director: String },
-    Audiobook { title: String },
-    Podcast(u32),
-    Placeholder
-}
+mod content;
 
-impl Media {
-    fn description(&self) -> String {
-        match self {
-            Media::Book { title, author } => {
-                format!("Book: {} by {}", title, author)
-            },
-            Media::Movie { title, director } => {
-                format!("Movie: {} by {}", title, director)
-            },
-            Media::Audiobook { title } => {
-                format!("Audiobook: called {}", title)
-            },
-            Media::Podcast(episode) => {
-                format!("Podcast: episode {}", episode)
-            },
-            Media::Placeholder => {
-                format!("Placeholder")
-            }
-        }
-    }
-}
-#[derive(Debug)]
-struct Catalog {
-    items: Vec<Media>
-}
-
-impl Catalog {
-    fn new() -> Self {
-        Catalog { items: vec![] }
-    }
-
-    fn add(&mut self, media: Media) {
-        self.items.push(media);
-    }
-
-    fn get_by_index(&self, index: usize) -> Option<&Media> {
-        if self.items.len() > index {
-            Some(&self.items[index])
-        } else {
-            None
-        }
-        
-    }
-}
-
-// fn print_media(media: Media) {
-//     println!("{:#?}", media)
-// }
+use content::media::Media;
+use content::catalog::Catalog;
 
 fn main() {
     let audiobook = Media::Audiobook { 
@@ -77,10 +24,6 @@ fn main() {
     // println!("{}", movie.description());
     // println!("{}", book.description());
     // println!("{}", audiobook.description())
-
-    // print_media(audiobook);
-    // print_media(movie);
-    // print_media(book);
 
     catalog.add(audiobook);
     catalog.add(movie);
